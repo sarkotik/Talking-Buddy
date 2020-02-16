290ba75a3bd7e67814e36a768dde8dfa6f8e7ee3
sarkotik <brianyuliu@gmail.com>
2020-02-16 12:08:47 -0800

    Update handle_data.py

--- handle_data.py
+++ handle_data.py
@@ -5,10 +5,8 @@ def load_data(filename):
     print("Loading the file: " + filename + "...\n")
     with open(filename, 'r') as jfile:
         data = json.load(jfile) # decodes the json file into a list
+    return data
 
-    print_json_data(data) # custom function written to actually be able to read the json file lol
-    writeDataIntoTxt(data)
-        
 def print_json_data(data): # type of data is a list of dictionaries
     print('\nTotal number of dialogues extracted =', len(data))
     for d in data: # iterate through the outer list, each d is a dictionary
@@ -23,12 +21,35 @@ def print_json_data(data): # type of data is a list of dictionaries
         
         print("-------------------------------------------------------------------------------------------------------------------\n-------------------------------------------------------------------------------------------------------------------\n")
 
+def extract_sentence_pairs(data): # extract sentence pairs
+    pairs = [] # final data structure to be returned. list of sentence pairs
+    for d in data: # for each dictionary in the list (each convo)
+        for i in range(0, len(d['dialog'])): # for each dictionary in the dialog key (list)
+            if i < (len(d['dialog']) - 1):  # if less than the last elm
+                  pairs.append((d['dialog'][i]['text'], d['dialog'][i+1]['text']))
+    return pairs
+
+def print_sentence_pairs(pairs): # prints the extracted sentence pairs
+    for i in pairs:
+        print("0 -> " + str(i[0]) + "\n1 -> " + str(i[1]) + "\n")
+        
+def handle_data(filename): # puts all of our functions together in handling data
+    print("\nLoading Data\n")
+    data = load_data(filename) # first, load our json data into our process 
+    #print_json_data(data) # custom function written to actually be able to read the json file lol
+    print("Reformatting Data into Sentence Pairs\n")
+    pairs = extract_sentence_pairs(data) # returns a list of sentence pairs stripped from each conversation
+    print("Sentence Pairs Extracted\n")
+    #print_sentence_pairs(pairs)
+
+
 #convert json data to txt data
-def writeDataIntoTxt(data):
+'''
+    def writeDataIntoTxt(data):
     # utterance data file
-    f1 = open("./utteracne.txt", "w")
+    f1 = open("./data/utterance.txt", "w")
     # conversation data file
-    f2 = open("./conversation.txt", "w")
+    f2 = open("./data/conversation.txt", "w")
     # id counter
     counter = 0
     # utterance list
@@ -55,7 +76,7 @@ def writeDataIntoTxt(data):
                 counter += 1
             dialogUtterances = dialogUtterances + str(index) + ","
             utteranceData = str(index) + "+++$+++" + sender + "+++$+++" + v.get("text") + " \n"
-            print(utteranceData)
+            #print(utteranceData)
             f1.write(utteranceData)
         # get rid of the last ","
         dialogUtterances = dialogUtterances[:-1] + "]"
@@ -63,8 +84,4 @@ def writeDataIntoTxt(data):
         f2.write(conversationData)
     f1.close()
     f2.close()
-
-
-
-
-
+'''
