b8fb1f32cbe0b0fa5f54d597e2796f6a86a514bb
sarkotik <brianyuliu@gmail.com>
2020-02-18 13:44:35 -0800

    not done yet

--- handle_data.py
+++ handle_data.py
@@ -1,5 +1,8 @@
 import simplejson as json # will be needed to work with our json files
+import vocab  # our custom module/class to build our vocabulary with
+import re # regular expressions will be used for simple string replacements
 
+### -------- LOAD DATA -------- ### 
 def load_data(filename):
     # loads the data, currently is implemented to handle json input
     print("Loading the file: " + filename + "...\n")
@@ -21,6 +24,7 @@ def print_json_data(data): # type of data is a list of dictionaries
         
         print("-------------------------------------------------------------------------------------------------------------------\n-------------------------------------------------------------------------------------------------------------------\n")
 
+### -------- EXTRACT SENTENCE PAIRS FROM DATA -------- ### 
 def extract_sentence_pairs(data): # extract sentence pairs
     pairs = [] # final data structure to be returned. list of sentence pairs
     for d in data: # for each dictionary in the list (each convo)
@@ -32,56 +36,35 @@ def extract_sentence_pairs(data): # extract sentence pairs
 def print_sentence_pairs(pairs): # prints the extracted sentence pairs
     for i in pairs:
         print("0 -> " + str(i[0]) + "\n1 -> " + str(i[1]) + "\n")
-        
+
+### -------- PREPARE DATA AND BUILD VOCAB --------  ###
+MAX_LENGTH = 10 # maximum sentence length to be considered. can be changed
+
+def stringToAscii(s): # takes a string and converts each char to ascii
+    return ''.join(
+        c for c ini unicodedata.normalize('NFD', s)
+        if unicodedata.catagory(c) != 'Mn'
+    )
+
+def normalizeString(s)  # takes a string and lowercases, trims, and removes non-letter chars
+    # calls our stringToAscii function in order to convert to ascii
+    s = stringToAscii(s.lower().strip())  # lowercases and strips white spaces on the left and right of the string
+    s = re.sub(r"([.!?])", r" \1", s)  # gets rid of punctuation
+    s = re.sub(r"[^a-zA-Z.!?]+", r" ", s) # only allows these kind of chars
+    s = re.sub(r"\s+", r" ", s).strip() # strips remaining white spaces
+    return s
+
+### -------- HANDLE DATA MAIN -------- ### 
 def handle_data(filename): # puts all of our functions together in handling data
     print("\nLoading Data\n")
-    data = load_data(filename) # first, load our json data into our process 
+    data = load_data(filename) # first, load our json data into our process
     #print_json_data(data) # custom function written to actually be able to read the json file lol
+
     print("Reformatting Data into Sentence Pairs\n")
     pairs = extract_sentence_pairs(data) # returns a list of sentence pairs stripped from each conversation
-    print("Sentence Pairs Extracted\n")
     #print_sentence_pairs(pairs)
 
+    print("Preparing Data and Building Vocabulary")
+    # now that we have the pairs, build the vocab and alter pairs to fit what we want
 
-#convert json data to txt data
-'''
-    def writeDataIntoTxt(data):
-    # utterance data file
-    f1 = open("./data/utterance.txt", "w")
-    # conversation data file
-    f2 = open("./data/conversation.txt", "w")
-    # id counter
-    counter = 0
-    # utterance list
-    utterances = []
-    for d in data:
-        # the list of every utterances in a dialog
-        dialogUtterances = "["
-        # get participant1 id
-        participant1 = d.get("participant1_id").get("user_id")
-        #get participant2 id
-        participant2 = d.get("participant1_id").get("user_id")
-        dialog = d.get("dialog")
-        # assign sender's and receiver's id
-        sender = participant1 if d.get("sender") == participant1 else participant2
-        receiver = participant2 if d.get("sender") == participant1 else participant1
-        for v in dialog:
-            # find whether the utterance exist
-            if v.get("text") in utterances:
-                index = utterances.index(v.get("text"))
-            else:
-            # not exist create a new id
-                utterances.append(v.get("text"))
-                index = counter
-                counter += 1
-            dialogUtterances = dialogUtterances + str(index) + ","
-            utteranceData = str(index) + "+++$+++" + sender + "+++$+++" + v.get("text") + " \n"
-            #print(utteranceData)
-            f1.write(utteranceData)
-        # get rid of the last ","
-        dialogUtterances = dialogUtterances[:-1] + "]"
-        conversationData = sender + "+++$+++" + receiver + "+++$+++" + dialogUtterances + " \n"
-        f2.write(conversationData)
-    f1.close()
-    f2.close()
-'''
+
